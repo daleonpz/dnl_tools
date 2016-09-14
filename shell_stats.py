7628c6fe78961d879594c3876ca8301e22b4f806
Daniel Paredes <danielp@wolfram.com>
2016-09-14 16:49:14 -0500

    shell_stats: working in history

--- shell_stats.py
+++ shell_stats.py
@@ -2,20 +2,36 @@
 
 import re # Regex
 
-log = [ line.rstrip('\n') for line in  open('/var/log/pacman.log','r')]
+#log = [ line.rstrip('\n') for line in  open('/var/log/pacman.log','r')]
 
-tags = []
-for line in log:
-    tags += re.findall('\[[A-Z\-]+\]', line) 
+#tags = []
+#for line in log:
+#    tags += re.findall('\[[A-Z\-]+\]', line) 
+#tags = list(set(tags))
+
+#print tags
+
+
+file = open('/var/log/pacman.log','r').read()
+
+# get the metadata and tags
+metadata = re.findall('\[[0-9\-\s:]+\]\s+\[[A-Z\-]+\]', file)
+tags = re.findall('\[[A-Z\-]+\]', file)
+
+counts = [  tags.count(x)  for x in  list(set(tags))  ]
 tags = list(set(tags))
 
-print tags
+dictionary = dict(zip(tags, counts))
 
+print dictionary 
 
-file = open('/var/log/pacman.log','r')
-# get the dates and tags
-tags = re.findall('\[[0-9\-\s:]+\]\s+\[[A-Z\-]+\]', file.read())
+#os.popen('HISTTIMEFORMAT="%d/%m/%y %T "').read()
 
-print set(tags)
-
-file.close ()
+import subprocess
+#p = subprocess.Popen(["history","16"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT )
+p = subprocess.Popen(["ps","-a"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT )
+#(output , err) = p.communicate()
+#print output
+for line in p.stdout.readlines():
+    print line
+retval = p.wait()
