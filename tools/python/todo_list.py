a49a25c2ccd785bdd97df280a8b9086d42164824
Daniel Paredes <danielp@wolfram.com>
2017-01-10 17:40:38 -0500

    display message when no opts

--- tools/python/todo_list.py
+++ tools/python/todo_list.py
@@ -6,6 +6,7 @@
 # - add new jobs
 # - mark jobs as done
 # - query jobs from headers
+# - format headers
 
 ################################
 #   I M P O R T S
@@ -29,12 +30,14 @@ def list_func(cls):
 def select_func(key, cls):
     return {
             'headers'   : cls.headers(),
-            'test'      : cls.test()
+            'getjob'    : cls.test()
             }.get(key, list_func(todo) )
 
 def usage():
-    print "Valid inputs:"
-    print "-f, --function:\n\t" + list_func(todo)
+    print "Usage:"
+    print "\t-h, --help\t\t: display help"
+    print "\t-H, --headers\t\t: display available headers in your todolist.org" 
+    print "\t-j, --jobs [header]\t: retrieve jobs from a specific header"
 
 
 ################################
@@ -48,10 +51,10 @@ class todo(object):
 
     def headers(self):
         # need to delete the last whitespace
-        return re.findall("\*\* ([A-Za-z\s-]+)", self.string)
+        print re.findall("\*\* ([A-Za-z\s-]+)", self.string)
 
-    def test(self):
-        return "test"
+    def job_retrieval(self, arg):
+        print arg
 
 
 ################################
@@ -60,16 +63,21 @@ class todo(object):
 def main():
     dolist = todo()
     try:
-        opts, args = getopt.getopt(sys.argv[1:] , "hf:", ["function="]  )
+        opts, args = getopt.getopt(sys.argv[1:] , "hHj:", ["jobs="]  )
     except getopt.GetoptError:
-        print 'todo_list.py -f <function>'
+        usage()
         sys.exit(2)
+    if (opts == []):
+            usage()
+            sys.exit()
     for opt, arg in opts:
         if opt in ('-h', '--help'):
             usage()
             sys.exit()
-        elif opt in ( '-f', '--function'):
-            print select_func(arg, dolist)
+        elif opt in ( '-H', '--headers'):
+            dolist.headers()
+        elif opt in ( '-j', '--jobs'):
+            dolist.job_retrieval(arg)
         else:
             assert False, "unhandled option"
 
