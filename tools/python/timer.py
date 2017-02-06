3fa7be77a75ae3ebad3716fb82c5eff7e462cf40
Daniel Paredes <daleonpz@gmail.com>
2017-02-05 20:08:20 -0500

    changed parser management, added new buttons and style

--- tools/python/timer.py
+++ tools/python/timer.py
@@ -2,10 +2,6 @@
 ###############################
 #  T O   D O
 ###############################
-# - close automatically
-# - larger fonts
-# - sound as output
-# - add titles
 # - multiset timer (future)
 
 ################################
@@ -16,6 +12,9 @@ from Tkinter import *
 import sys
 import re
 import getopt
+import pygame
+import argparse
+
 ################################
 #   F U N C T I O N S
 ################################
@@ -34,8 +33,10 @@ class Countdown(Label):
             t = map(int,re.split(":",timer))
             self.value = t[1] + t[0]*60
             t ="{:02d}:{:02d}".format(*divmod( self.value, 60)) 
-            Label.__init__(self, parent, width=5, text=t)
+            Label.__init__(self, parent, width=5, text=t, font=('Helvetica','20'))
             self._job_id = None
+            pygame.init()
+            pygame.mixer.music.load("timer.wav")
 
         def tick(self):
             self.value -= 1
@@ -44,62 +45,59 @@ class Countdown(Label):
             if self.value > 0:
                 self._job_id = self.after(1000, self.tick)
 
+            if self.value == 0: self.stop_loop()
+
         def start(self):
             if self._job_id is not None: return
 
             self.stop_requested = False
             self.after(1000, self.tick)
 
-        def stop(self):
-            self.after_cancel(self._job_id)
-            self._job_id = None
+        def stop_loop(self):
+            if not( self.stop_requested ):
+                pygame.mixer.music.play()
+                self.after(5000, self.stop_loop)
 
 class Application(Frame):
-    def createWidgets(self,timer):
+    def createWidgets(self, timer, header):
         label =  Countdown(self, timer)
-        label.pack(side=TOP)
-        
+        label.config( height = 2) 
+        label.pack(side=LEFT)
         label.start()
 
-        self.QUIT = Button(self)
-        self.QUIT["text"] = "QUIT"
-        self.QUIT["command"] =  self.quit
+        title = Label(self, text=header.upper(), font=('Helvetica','20', 'bold'));
+        title.config( height = 2 )
+        title.pack(side=TOP)
 
-        self.QUIT.pack(side=LEFT)
+        self.STOP = Button(self)
+        self.STOP["text"] = "STOP / QUIT"
+        self.STOP["command"] =  self.quit
+        self.STOP.config( height = 2, width = 20 )
+        self.STOP.pack(side=LEFT)
 
     # SyntaxError: non-default argument follows default argument
     # thus, variable timer cannot be after master=None
-    def __init__(self,timer, master=None):
+    def __init__(self,timer, header, master=None):
         Frame.__init__(self, master)
         self.pack()
-        self.createWidgets(timer)
+        self.createWidgets(timer, header)
 
 ################################
 #   M A I N
 ################################
 def main():
-    
-    try:
-        opts, args = getopt.getopt(sys.argv[1:] , "ht:", ["time="]  )
-    except getopt.GetoptError:
-        usage()
-        sys.exit(2)
-    if (opts == []):
-        usage()
-        sys.exit()
-    for opt, arg in opts:
-        if opt in ('-h', '--help'):
-            usage()
-            sys.exit()
-        elif opt in ( '-t', '--time'):
-            root = Tk()
-            app = Application( arg, master=root)
-            app.mainloop()
-            root.destroy()
-        else:
-            assert False, "unhandled option"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("time", help="set time following format 00:00")
+    parser.add_argument("-H","--header", help="task header", default="TASK")
 
+    args = parser.parse_args()
+
+    root = Tk()
+    root.title("T I M E R")
+    app = Application( args.time, args.header,  master=root)
+    app.mainloop()
+    root.destroy()
 
 if __name__ == "__main__":
-    main()
+    main2()
 
