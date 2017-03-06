e3b47abe2e282e7a60a72b2a33aa18d470ba476f
Daniel Paredes <daleonpz@gmail.com>
2017-03-06 17:01:18 -0500

    clean code

--- tools/cpp/imdbinterface.c
+++ tools/cpp/imdbinterface.c
@@ -7,22 +7,32 @@
 /*
  * TODO: 
  *      - use something similar to argparser (python)
+ *      - hidden password
+ *      - ask for possible actions to perform ( add, delete, display)
+ *      - add predefined queries ( display full table, one entry)
  *      - add data
  *      - delete data
  * */
 
 
 #define MAX_LENGTH 200
+static void exit_nicely(PGconn *conn)
+{
+    PQfinish(conn);
+    exit(1);
+}
 
-int     main(int argc, char* argv[]) {
+
+int main(int argc, char* argv[]) {
     PGconn          *conn;
     PGresult        *res;
     int             rec_count;
+    int             nFields;
     int             row;
     int             col;
  
-    char *conninput;
-    conninput = (char*) calloc(MAX_LENGTH, sizeof(char));
+    char *connparse;
+    connparse = (char*) calloc(MAX_LENGTH, sizeof(char));
 
     const char *dbuser, *dbpassword;
     dbuser = argv[1];
@@ -31,32 +41,37 @@ int     main(int argc, char* argv[]) {
     const char *dbname = "pucp";
     const char *dbhost = "localhost";
 
-    sprintf(conninput,"dbname=%s host=%s user=%s password=%s",dbname,dbhost,dbuser,dbpassword);
+    sprintf(connparse,"dbname=%s host=%s user=%s password=%s",dbname,dbhost,dbuser,dbpassword);
 
-    conn = PQconnectdb(conninput);
+    conn = PQconnectdb(connparse);
      
-    free(conninput);
+    free(connparse);
 
      if (PQstatus(conn) == CONNECTION_BAD) {
-             puts("We were unable to connect to the database");
-             exit(0);
+        fprintf(stderr, "Connection to database failed: %s",
+                PQerrorMessage(conn));
+        exit_nicely(conn);
      }
+     else 
+         puts("Connection is established!");
 
-     res = PQexec(conn,
-             "select * from parkour");
+     res = PQexec(conn, "select * from parkour");
+     /* sorted entries (codigo, nombre, email)*/
  
      if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-             puts("We did not get any data!");
-             exit(0);
+        fprintf(stderr, "FETCH ALL failed: %s", PQerrorMessage(conn));
+        PQclear(res);
+        exit_nicely(conn);
      }
 
      rec_count = PQntuples(res);
+     nFields = PQnfields(res);
 
      printf("We received %d records.\n", rec_count);
      puts("==========================");
  
      for (row=0; row<rec_count; row++) {
-             for (col=0; col<3; col++) {
+             for (col=0; col<nFields; col++) {
                      printf("%s\t", PQgetvalue(res, row, col));
              }
              puts("");
@@ -67,6 +82,8 @@ int     main(int argc, char* argv[]) {
      PQclear(res);
 
      PQfinish(conn);
+
+     puts("Connection is closed!");
  
      return 0;
  }
