a244eb7aaff41d7db3742be537437436bd7d8f41
Daniel Paredes <daleonpz@gmail.com>
2017-03-07 10:44:00 -0500

    add header to manage inputparse

---
+++ tools/cpp/parse.h
@@ -0,0 +1,17 @@
+#ifndef _PARSE_H_
+#define _PARSE_H_
+
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int getch();
+
+void getpass(char **pass);
+
+
+#endif
+ 
