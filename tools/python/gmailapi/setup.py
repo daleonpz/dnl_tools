312413417caf93fe5fe27a8ab788b6c76a5df06e
Daniel Paredes <daleonpz@gmail.com>
2017-03-20 15:06:54 -0500

    create a package gmailapi

---
+++ tools/python/gmailapi/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gmailapi',
+      version='0.1',
+      description='send mail via gmail',
+#      url='http://github.com/storborg/funniest',
+      author='Daniel Paredes',
+      author_email='daleonpz@gmail.com',
+#      license='MIT',
+      packages=['gmailapi'],
+      zip_safe=False)
